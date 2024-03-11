a4db97288895d060e1ef488ca6de15a466d96bd0
Thom_i <62951436+Tomdev77@users.noreply.github.com>
2024-03-11 23:08:48 +0100

    Delete src directory

--- src/components/menupart.jsx
+++
@@ -1,9 +0,0 @@
-export const Menupart = [ // tableau à récupérer et lier  => menupart.jsx with dropdownpart.jsx
-    {
-      title: 'Informations',
-      path: '/Informations',
-      Name: 'dropdown-link'
-    },
-
-  ];
-
--- src/components/menuprog.jsx
+++
@@ -1,9 +0,0 @@
-export const Menuprog = [// tableau à récupérer et lier  => menuprog.jsx with dropdown.jsx
-
-    {
-      title: 'Artistes',
-      path: '/Artistes',
-      Name: 'dropdown-link'
-    },
-
-  ];
